fa61cec552fbb905848785082c10bbbd31fd6c2d
Sam Vlas <samvlas@live.nl>
2020-05-13 10:00:18 +0200

    Arrow Functions!

---
+++ Week 2/Dag 3/W2D3-1/script.js
@@ -0,0 +1,26 @@
+//A
+const ikRockArrowFunctions = () => {
+    console.log("Joe, ik rock de arrow functions!");
+};
+
+ikRockArrowFunctions();
+
+//B
+const fivePlusSeven = () => 5 + 7;
+
+console.log(fivePlusSeven());
+
+//C
+() => 1 + 2;
+
+//D
+const myFunction = (a, b) => a + b;
+
+//E
+const addFive = a => a + 5;
+console.log(addFive(4));
+
+//F
+const createObject = () => ({ greeting: 'hoi' });
+
+
