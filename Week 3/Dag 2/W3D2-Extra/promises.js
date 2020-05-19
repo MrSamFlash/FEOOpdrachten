3dd20a4ec5040c5dc8187e75796a27b30163a654
Sam Vlas <samvlas@live.nl>
2020-05-19 15:47:50 +0200

    Promises

---
+++ Week 3/Dag 2/W3D2-Extra/promises.js
@@ -0,0 +1,19 @@
+/*
+Exercise 1:
+Write a function testNum that takes a number as an argument and returns a Promise that tests if the value is less than or greater than the value 10. Log the result to the console.
+*/
+
+
+const testNum = num => {
+    return new Promise((resolve, reject) => {
+        if (num >= 10) {
+            resolve(num + ' is greater than 10');
+        } else {
+            reject(num + ' is less than 10');
+        }
+    });
+};
+
+testNum(11)
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
